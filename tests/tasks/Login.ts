5f6c9859925a6ef2aed04e05c2661d71251ee617
Daniel Evans <danielevans305@gmail.com>
2024-04-24 01:35:44 +0100

    fixed error for robustness

--- tests/tasks/Login.ts
+++ tests/tasks/Login.ts
@@ -1,17 +1,10 @@
 import { Page } from 'playwright';
 import { fillLoginForm } from '@interactions/UserInteractions';
 
-export const loginWithCredentials = async (username: string, password: string, page: Page) => {
-    // Wait for login box to be visible
-    const loginBox = page.locator('.login-box');
-    await loginBox.waitFor();
-
-    // check if username and password are visible
-    const usernameInput = loginBox.locator('input[type="text"][data-test="username"]');
-    await usernameInput.waitFor();
-    const passwordInput = loginBox.locator('input[type="password"][data-test="password"]');
-    await passwordInput.waitFor();
-
-    // Fill username and password
-    await fillLoginForm(username, password, page);
-};
+export class Login {
+    static withCredentials(username: string, password: string) {
+        return async (page: Page): Promise<void> => {
+            await fillLoginForm(username, password, page);
+        };
+    }
+}
