75c7aef81c5ebd01f00e572a8afcb876e6faf503
Daniel Evans <danielevans305@gmail.com>
2024-04-26 04:46:57 +0100

    added page param requirement to ensure new test is implemented if running in parallel

--- tests/tasks/Login.ts
+++ tests/tasks/Login.ts
@@ -2,7 +2,7 @@ import { Page } from 'playwright';
 import { fillLoginForm } from '@interactions/UserInteractions';
 
 export class Login {
-    static withCredentials(username: string, password: string) {
+    static withCredentials(username: string, password: string, page: Page) {
         return async (page: Page): Promise<void> => {
             await fillLoginForm(username, password, page);
         };
